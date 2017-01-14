93b1ee0de534404e4de3d7dc5643bf20356b39c0
ampersand <ampersand@underscore.space>
2017-01-14 19:26:00 +0000

    Moved setTimeout to parent for greater efficiency

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 angular.module("onScreenApp", [])
 
-.controller("onScreenCtrl", function($scope, $rootScope, $timeout) {
+.controller("onScreenCtrl", function($scope) {
   $scope.items = [];
-  for (i = 1; i <= 3000; i++) {
+  for (i = 1; i <= 300; i++) {
     $scope.items.push({
       "title": "Mambo number " + i,
       "index": i,
@@ -11,38 +11,19 @@ angular.module("onScreenApp", [])
   }
 })
 
-.directive('listouter', function($rootScope) {
+.directive('listouter', function() {
   return {
     restrict: 'C',
     compile: function() {
-      return function(scope, element, attributes) {
-        element.on('scroll', function() {
-          $rootScope.$broadcast('scrolled', element[0]);
-          scope.$digest();
-        });
-      }
-    }
-  };
-})
-
-.directive("listitem", function() {
-  return {
-    restrict: "C",
-    controller: function($scope) {      
-      $scope.item.visible = function(isVisible) {
-        $scope.item.isVisible = isVisible;
-        if (isVisible) {
-          $scope.item.seen = true;
-        }
-        $scope.$digest();
-      }
-    },
-    compile: function() {
       return {
-        pre: function(scope, elem, attrs) {
-          var ngVis = {
-            _timer: null,
+        post: function(scope, element, attributes) {
+          var ngVisO = {
+          _timer: null,
             _delay: 300,
+            _check: function() {
+              scope.$broadcast('scrolled', element[0]);
+              scope.$digest();
+            },
             check: function(e, p) {
               var self = this;
               if (this._timer) {
@@ -53,8 +34,34 @@ angular.module("onScreenApp", [])
                   self._check(e, p);
                   self._timer = null;
               }, this._delay);
-            },
-            _check: function(e, p) {
+            }
+          }
+          element.bind('scroll', function () {
+            ngVisO.check();
+          });
+          ngVisO.check();
+        }
+      }
+    }
+  };
+})
+
+.directive("listitem", function() {
+  return {
+    restrict: "C",
+    controller: function($scope) {
+      $scope.item.visible = function(isVisible) {
+        $scope.item.isVisible = isVisible;
+        if (isVisible) {
+          $scope.item.seen = true;
+        }
+      }
+    },
+    compile: function() {
+      return {
+        pre: function(scope, elem, attrs) {
+          var ngVisI = {
+            check: function(e, p) {
               eTop = e.offsetTop;
               eBot = e.offsetTop + e.offsetHeight;
               sTop = p.scrollTop;
@@ -64,15 +71,12 @@ angular.module("onScreenApp", [])
             }
           }
 
-          scope.$on('scrolled', function() {
-              ngVis.check(elem[0], elem.parent()[0]);
+          scope.$on('scrolled', function(ev, p) {
+              ngVisI.check(elem[0], p);
           });
           scope.$on("destroy", function(e) {
               clearTimeout(this._timer);
           });
-        },
-        post: function(scope, elem, attrs) {
-          scope.$broadcast('scrolled', elem[0]);
         }
       }
     }
